e26b68a817e8443160cf63228abe8101258de432
Donovan <z8phyr@hotmail.com>
2023-09-26 08:41:15 -0700

    Implement CRUD Operations

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Email
 
 
@@ -17,3 +17,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class BlogPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    # You can further improve how tags are inputted and processed.
+    tags = StringField('Tags')
+    submit = SubmitField('Post')
--- routes.py
+++ routes.py
@@ -1,14 +1,15 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask import render_template, redirect, url_for, flash, request, abort
+from flask_login import login_user, logout_user, login_required, current_user
 from myblog import app, db
-from myblog.models import User
-from myblog.forms import RegistrationForm, LoginForm
+from myblog.models import User, Post
+from myblog.forms import RegistrationForm, LoginForm, BlogPostForm
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    posts = Post.query.all()
+    return render_template('home.html', title='Home', posts=posts)
 
 
 @app.route('/about')
@@ -46,3 +47,55 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+def new_post():
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        # or wherever you want to redirect after creating a post.
+        return redirect(url_for('home'))
+    return render_template('create_post.html', form=form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('home', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('update_post.html', form=form, post=post)
+
+
+# Only POST for security reasons
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
